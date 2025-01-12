76f502d66fb9864041981f400ccc5fe7ffef7548
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-01-12 20:46:03 +0200

    update

--- js/game_X-0.js
+++ js/game_X-0.js
@@ -1,2 +1,9 @@
 const container = document.querySelector("js-content");
 console.log(container);
+let markup = '';
+
+for (let i = 0; i < 9; i++) {
+markup += `<div class = "item"></div>;`    
+}
+
+container.innerHTML = markup;
